49c545abf01f6bcc18554c085f22af238d31f076
mary <mary123@gmail.com>
2025-08-27 00:02:30 +0530

    changes

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,14 @@
+// models/User.js
 const { DataTypes } = require("sequelize");
-const sequelize = require("../config/db");
+const sequelize = require("../config/db"); 
 
 const User = sequelize.define("User", {
   name: { type: DataTypes.STRING, allowNull: false, defaultValue: "User" },
   avatar: { type: DataTypes.STRING, allowNull: true },
   budget: { type: DataTypes.FLOAT, allowNull: false, defaultValue: 0 }
+}, {
+  timestamps: true,       
+  freezeTableName: true   
 });
 
 module.exports = User;
